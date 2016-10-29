06f415050c9a0e5676fc83fe635292c1070e7c4d
arielmagbanua <magbanua.c.ariel@gmail.com>
2016-10-30 05:09:51 +0800

    Added database seeder for Users model

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $member = User::firstOrNew([
+        		'email' => 'ariel@amazing.com'
+        	]);
+
+        $member->first_name = 'Ariel';
+        $member->middle_name = 'Cacanog';
+        $member->first_name = 'Magbanua';
+        $member->address = '8th Avenue, Bangoy Street, Brgy. Tomas Monteverde, Agdao, Davao City';
+        $member->birth_date = '1988-04-23';
+        $member->account_type = 2;
+        $member->password = bcrypt('password');
+        $member->save();
+
+        $admin = User::firstOrNew([
+        		'email' => 'admin@amazing.com'
+        	]);
+
+        $admin->first_name = 'John';
+        $admin->middle_name = 'Amazing';
+        $admin->first_name = 'Doe';
+        $admin->address = '8th Avenue, Bangoy Street, Brgy. Tomas Monteverde, Agdao, Davao City';
+        $admin->birth_date = '1988-04-23';
+        $admin->account_type = 1;
+        $admin->password = bcrypt('password');
+        $admin->save();
+
+    }
+}
