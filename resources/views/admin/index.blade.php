383a3c43aac4591b4225d95d0e00f73c2c6838e7
arielmagbanua <magbanua.c.ariel@gmail.com>
2016-11-01 20:00:31 +0800

    Created index blade template for admin and member portal.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('header-links')
+@endsection
+
+@section('main-content')
+    Admin
+@endsection
+
+@yield('footer-links')
+@endsection
---
+++ resources/views/member/index.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@extends('master')
+
+@section('header-links')
+@endsection
+
+@section('main-content')
+    Member
+@endsection
+
+@yield('footer-links')
+@endsection
